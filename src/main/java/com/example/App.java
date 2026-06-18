24744df68ceeba9c2628da014134d0c9103b7846
yashvisharma741-arch <yashvisharma741@gmail.com>
2026-06-18 20:34:32 +0530

    Initial Commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://saucedemo.com/");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+    }
+}
